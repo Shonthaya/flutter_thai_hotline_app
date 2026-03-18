ef0f0d5362f24501dc3dd4985cf505d3d1bccb35
s6752410004@sau.ac.th <s6752410004@sau.ac.th>
2026-03-18 19:55:51 +0700

    M1

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:fflutter_thai_hotline_app/views/home_ui.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(FlutterThaiHotLineApp());
+}
+
+//=================================================
+
+class FlutterThaiHotLineApp extends StatefulWidget {
+  @override
+  State<FlutterThaiHotLineApp> createState() => _FlutterThaiHotLineAppState();
+}
+
+class _FlutterThaiHotLineAppState extends State<FlutterThaiHotLineApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeUi(),
+      theme: ThemeData(
+        textTheme: GoogleFonts.kanitTextTheme(
+          Theme.of(context).textTheme,
+        ),
+      ),
+    );
+  }
+}
